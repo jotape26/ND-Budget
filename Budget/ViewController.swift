a8b1945fe3190b380dbc69b3c208acfb05efb334
João Leite <joaopleite26@gmail.com>
2019-02-05 23:45:13 -0200

    Initial Commit

---
+++ Budget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Budget
+//
+//  Created by João Leite on 05/02/19.
+//  Copyright © 2019 João Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
