d0b2114080344f0b7533d87f91627ed78992ddde
João Leite <joaopleite26@gmail.com>
2019-02-06 00:17:56 -0200

    Firebase/Firestore initial test and setup

--- Budget/ViewController.swift
+++ Budget/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
-
+    
+    var db: Firestore?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        db = Firestore.firestore()
     }
-
-
+    
+    @IBAction func salvarBtnClicked(_ sender: Any) {
+        
+        guard let db = db else { return }
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("expenses").addDocument(data: [
+            "name": "Compra Mac Mini",
+            "date": Date().description,
+            "value": 7500.50,
+            "map": ["test":"test"]
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+        db.collection("expenses").
+            
+        
+    }
+    
 }
 
